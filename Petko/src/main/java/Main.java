8d4660a514eef044cba3ecff341a210b1bec644e
Mino <m.karadjov@icloud.com>
2021-12-07 22:49:09 +0100

    SelectionSort Fix + Enumerations relocated.
    - Enums relocated into AlgorithmTester
    - AnalysisReport with results for Selection and QuickSort.
    - SelectionSort fixed

--- Petko/src/main/java/Main.java
+++ Petko/src/main/java/Main.java
@@ -1,18 +1,36 @@
 import algorithms.*;
-import utils.AlgorithmTester;
-import utils.ReportWizard;
+import utils.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        AlgorithmTester tester = new AlgorithmTester(true);
+        AlgorithmTester tester = new AlgorithmTester(false);
         ReportWizard reportWizard = new ReportWizard();
+        List<AnalysisReport> reports = new ArrayList<>();
 
-        tester.setSorter(new BubbleSort());
-        var results = tester.executeTest(300);
-        System.out.println(results);
-        reportWizard.saveReport(results);
+        //reports.addAll(testRun(AlgorithmTester.SorterType.QUICKSORT, tester));
+        //reports.addAll(testRun(AlgorithmTester.SorterType.HEAPSORT, tester));
+        //reports.addAll(testRun(AlgorithmTester.SorterType.SELECTIONSORT, tester));
+       // reports.addAll(testRun(AlgorithmTester.SorterType.BUBBLESORT, tester));
+       // reports.addAll(testRun(AlgorithmTester.SorterType.BOGOSORT, tester));
+       reports.add(tester.executeTest(AlgorithmTester.SorterType.SELECTIONSORT, AlgorithmTester.CollectionSize.SPACIOUS));
+       reports.add(tester.executeTest(AlgorithmTester.SorterType.QUICKSORT, AlgorithmTester.CollectionSize.LARGE));
+        reportWizard.saveReports(reports);
+
+    }
+
+    public static List<AnalysisReport> testRun(AlgorithmTester.SorterType sorterForTesting, AlgorithmTester tester){
+        var returnableList = new ArrayList<AnalysisReport>();
+        var allSizeValues = AlgorithmTester.CollectionSize.values();
+
+        for(int i = 0; i < allSizeValues.length; i++){
+            returnableList.add(tester.executeTest(sorterForTesting,allSizeValues[i]));
+        }
+        return returnableList;
     }
 }
--- Petko/src/main/java/algorithms/SelectionSort.java
+++ Petko/src/main/java/algorithms/SelectionSort.java
@@ -8,17 +8,18 @@ public class SelectionSort implements Algorithm{
 
     void sort(int arr[])
     {
-        int n = arr.length;
-        for (int i = 0; i < n-1; i++)
+        for(int i=0; i<arr.length; i++)
         {
-            int min_idx = i;
-            for (int j = i+1; j < n; j++)
-                if (arr[j] < arr[min_idx])
-                    min_idx = j;
-
-            int temp = arr[min_idx];
-            arr[min_idx] = arr[i];
-            arr[i] = temp;
+            for(int j =i+1; j<arr.length; j++)
+            {
+                if(arr[i] > arr[j])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
         }
+
     }
 }
